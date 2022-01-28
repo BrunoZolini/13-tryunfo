92e6ae4f82255e4612afd774400354216b0a51c6
Bruno Zolini <bruno.zolini@hotmail.com>
2022-01-28 19:08:29 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ class App extends React.Component {
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
       cardImage: '',
       cardRare: 'normal',
       cardTrunfo: false,
@@ -24,6 +24,40 @@ class App extends React.Component {
     value = (type === 'checkbox') ? checked : value;
     this.setState({
       [name]: value,
+    }, () => { this.handleError(); });
+  }
+
+  handleError = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+    } = this.state;
+
+    const number1 = Number(cardAttr1);
+    const number2 = Number(cardAttr2);
+    const number3 = Number(cardAttr3);
+    const maxSum = 210;
+    const maxAttr = 90;
+    const minAttr = 0;
+
+    const errorCases = [
+      !cardName.length,
+      !cardImage.length,
+      !cardDescription.length,
+      (number1 > maxAttr || number1 < minAttr),
+      (number2 > maxAttr || number2 < minAttr),
+      (number3 > maxAttr || number3 < minAttr),
+      number1 + number2 + number3 > maxSum,
+    ];
+
+    const formularioPreenchido = errorCases.some((error) => error === true);
+
+    this.setState({
+      isSaveButtonDisabled: formularioPreenchido,
     });
   }
 
